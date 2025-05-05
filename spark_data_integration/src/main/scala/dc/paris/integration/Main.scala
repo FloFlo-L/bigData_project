ea1c141e159fc467101345aaad8bb01d84219da3
f_lescribaa <florian.lescribaa@gmail.com>
2025-05-05 09:20:08 +0200

    tp1

--- spark_data_integration/src/main/scala/dc/paris/integration/Main.scala
+++ spark_data_integration/src/main/scala/dc/paris/integration/Main.scala
@@ -2,7 +2,7 @@ package dc.paris.integration
 
 import org.apache.spark.sql.SparkSession
 
-import java.io.File
+//import java.io.File
 
 
 
@@ -11,8 +11,8 @@ object Main extends App {
     .builder()
     .appName("Data Integration")
     .master("local[*]")
-    .config("fs.s3a.access.key", "feYIrRkftrx645QsECKw") // A renseigner
-    .config("fs.s3a.secret.key", "2RDLb99JSmwd8Zj1JcNPvK6t2LbaQnHSDp1cctRW") // A renseigner
+    .config("fs.s3a.access.key", "RAPFJEDEXlliIdJuG2df") // A renseigner
+    .config("fs.s3a.secret.key", "sVIfgM5Sa2d23pNCEKfx2AyQVsUUuwBUyN24za57") // A renseigner
     .config("fs.s3a.endpoint", "http://localhost:9000/")
     .config("fs.s3a.path.style.access", "true")
     .config("fs.s3a.connection.ssl.enable", "false")
@@ -20,4 +20,21 @@ object Main extends App {
     .config("fs.s3a.connection.establish.timeout", "1000")
     .config("fs.s3a.connection.timeout", "5000")
     .getOrCreate()
+
+
+  // import spark.implicits._
+
+  def showAndUpload(inputPath: String, outputPath: String): Unit = {
+    // read file
+    val file = spark.read.parquet(inputPath)
+    file.show()
+    // upload file in minio
+    file.write
+      .mode("overwrite")
+      .parquet(outputPath)
+  }
+
+  showAndUpload("../data/raw/yellow_tripdata_2024-10.parquet", "s3a://spark/yellow_tripdata_2024-10.parquet")
+  showAndUpload("../data/raw/yellow_tripdata_2024-11.parquet", "s3a://spark/yellow_tripdata_2024-11.parquet")
+  showAndUpload("../data/raw/yellow_tripdata_2024-12.parquet", "s3a://spark/yellow_tripdata_2024-12.parquet")
 }
